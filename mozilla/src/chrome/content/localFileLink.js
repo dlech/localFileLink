4938154e42ff3f8c54ece811580b9472ff1365b3
David Lechner <david@lechnology.com>
2012-07-15 21:48:31 -0700

    changed mozilla extension to be more like IE counterpart. now asks if user wants to try to open parent dir if file does not exist.

--- mozilla/src/chrome/content/localFileLink.js
+++ mozilla/src/chrome/content/localFileLink.js
@@ -2,67 +2,46 @@
 
 var mozLocalFileLink = {
   
-  onPageLoad: function (aEvent) {
-    doc = aEvent.originalTarget;  
-    this.element = doc.getElementById("localFileLinkElement");
-    alert(this.element);
-  },  
-  
-  openFileFolderEventHandler : function (aEvent) {   
+  openFileFolderEventHandler : function (aEvent) { 
     let path = aEvent.target.getAttribute("path")  
     try {      
       this.showFile(path);
-    } catch (ex) {
-      
+    } catch (ex) {      
       alert("Error while opening '" + path + "'" + "\n\n" + ex);
     }
   },  
   
-  queryBrowserEventHandler : function (aEvent) {
+  queryBrowserEventHandler : function (aEvent) { 
     let element = aEvent.target;
-    element.setAttribute("browser", "mozilla");
+    element.setAttribute("extensiontype", "mozilla");
   },
-  
-  // this method is from mozillas download handler (original name: showDownload)
-  // http://mxr.mozilla.org/mozilla-release/source/toolkit/mozapps/downloads/content/downloads
+    
   showFile : function (aPathOrUrl) {
-    var file = this.getLocalFileFromNativePathOrUrl(aPathOrUrl);
-
-    try {
-      // Show the directory containing the file and select the file
-      if (!file.exists()) {
-        throw "File does not exist";
-      }
-      file.reveal();
-    } catch (e) {
-      // If reveal fails for some reason (e.g., it's not implemented on unix or
-      // the file doesn't exist), try using the parent if we have it.
-      let parent = file.parent.QueryInterface(Ci.nsILocalFile);
-      if (!parent || !parent.exists()) {
-        alert(aPathOrUrl + " does not exist.");
-        return;
-      }
-
-      try {
-        // "Double click" the parent directory to show where the file should be
-       parent.launch();
-      } catch (e) {
-        // If launch also fails (probably because it's not implemented), let the
-        // OS handler try to open the parent
-        this.openExternal(parent);
+    let file = this.getLocalFileFromNativePathOrUrl(aPathOrUrl);    
+    if (!file || !file.exists()) {
+      let prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]  
+        .getService(Components.interfaces.nsIPromptService); 
+      let flags = prompts.STD_YES_NO_BUTTONS;
+      let button0Text = "";
+      let button1Text = "";
+      let button2Text = "";
+      let checkboxText = null;
+      let showCheckbox = { value : false };
+      let retVal = prompts.confirmEx(null, "Error", "Path '" + aPathOrUrl +
+        "' does not exist.\n\n" + "Would you like to open the parent directory?", 
+        flags, button0Text, button1Text, button2Text, checkboxText, showCheckbox);
+      if (retVal == 0) { // user clicked yes
+        file = file.parent.QueryInterface(Components.interfaces.nsILocalFile);
+        if (!file || !file.exists()) {
+          prompts.alert(null, "Error", "Directory '" + file.path + "' does not exist.");
+          return;
+        }
       }
     }
-  },
-  
-  openExternal : function (aFile) {
-    var uri = Components.classes["@mozilla.org/network/io-service;1"].
-      getService(Components.interfaces.nsIIOService).newFileURI(aFile);
-
-    var protocolSvc = Components.classes["@mozilla.org/uriloader/external-protocol-service;1"].
-      getService(Components.interfaces.nsIExternalProtocolService);
-    protocolSvc.loadUrl(uri);
-
-    return;
+    try {
+    // Show the directory containing the file and select the file
+      file.reveal();
+    } catch (e) { }
   },
   
   // this method is from mozillas download handler
